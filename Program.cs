1401e65f747cc52ad0bf00454ff245afd68e0be9
Michael Robards <mikerobards@icloud.com>
2021-08-30 15:30:58 -0500

    finished lesson

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace files_module
 {
@@ -9,15 +10,23 @@ namespace files_module
         static void Main(string[] args)
         {
             var currentDirectory = Directory.GetCurrentDirectory();
-
             var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+            var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+            Directory.CreateDirectory(salesTotalDir);
 
             var salesFiles = FindFiles(storesDirectory);
 
-            foreach (var file in salesFiles)
-            {
-                Console.WriteLine(file);
-            }
+            var salesTotal = CalculateSalesTotal(salesFiles);
+
+            File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+
+
+            // foreach (var file in salesFiles)
+            // {
+            //     Console.WriteLine(file);
+            // }
 
         }
         static IEnumerable<string> FindFiles(string folderName)
@@ -38,6 +47,33 @@ namespace files_module
             }
 
             return salesFiles;
+        }
+
+        static double CalculateSalesTotal(IEnumerable<string> salesFiles)
+        {
+            double salesTotal = 0;
+
+            // Loop over each file path in salesFiles
+            foreach (var file in salesFiles)
+            {
+                // Read the contents of the file
+                string salesJson = File.ReadAllText(file);
+
+                // Parse the contents as JSON
+                SalesData data = JsonConvert.DeserializeObject<SalesData>(salesJson);
+
+                // Add the amount found in the Total field to the salesTotal variable
+                salesTotal += data.Total;
+            }
+
+            return salesTotal;
+        }
+
+
+
+        class SalesData
+        {
+            public double Total { get; set; }
         }
     }
 }

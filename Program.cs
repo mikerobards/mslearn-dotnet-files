ecd198e6b3260e324ce5df09ab05c29e2d9b0514
Michael Robards <mikerobards@icloud.com>
2021-08-30 14:54:27 -0500

    Lesson 5

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace files_module
 {
@@ -6,7 +8,36 @@ namespace files_module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+            var salesFiles = FindFiles(storesDirectory);
+
+            foreach (var file in salesFiles)
+            {
+                Console.WriteLine(file);
+            }
+
+        }
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                // The file name will contain the full path, so only check the end of it
+                var extension = Path.GetExtension(file);
+
+                if (extension == ".json")
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
         }
     }
 }
